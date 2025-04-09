b4a903e85b7a9ad5c2641f266aff756c8210de47
the-parvez-16 <parvez.k.m.16@gmail.com>
2025-04-10 00:27:01 +0530

    0.0.1

---
+++ src/scripts/script.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.querySelector('#menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+toggleBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
